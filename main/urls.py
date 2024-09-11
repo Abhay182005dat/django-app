6099c3ac97c4c17c45d260bcd58950a8c664e0ab
Abhay S N <abhay182005@gmail.com>
2024-09-11 15:56:36 +0530

    Done

--- main/urls.py
+++ main/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('add/' ,views.add_numbers , name = 'add_numbers' ),
+    path('pnr/' ,views.pnr, name = 'pnr' ),
 ]
--- main/views.py
+++ main/views.py
@@ -16,3 +16,19 @@ def add_numbers(request):
            error = "Something went wrong"
     return render(request, 'add.html' , {'result':result , 'error':error})
 
+def pnr(request):
+   
+  result  = None
+  error = None
+
+  if request.method == "POST":
+      num1 = request.POST.get('num1')
+      num2 = request.POST.get('num2')
+      num3 = request.POST.get('num3')
+
+      try:
+         si = (float(num1)*float(num2)*float(num3)) / 100
+         result = f"The Simple Interest is {si}"
+      except:
+          error = "Something went wrong"
+  return render(request, 'pnr.html' , {'result':result , 'error':error})
